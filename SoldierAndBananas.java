dd4875a719736f940ce3344d0ef639e61b657478
Vedant Garg <71409503+cryptopig@users.noreply.github.com>
2024-10-18 15:26:48 -0700

    Update and rename src/easy/Basketball1on1.java to SoldierAndBananas.java

---
+++ SoldierAndBananas.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class SoldierAndBananas {
+    public static void main(String[] args ) {
+        Scanner input = new Scanner(System.in);
+        int cost_i = input.nextInt();
+        int owned = input.nextInt();
+        int total = input.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= total; i++) {
+            sum += i;
+        }
+        
+        System.out.println((sum*cost_i) - owned);
+        
+    }
+}
