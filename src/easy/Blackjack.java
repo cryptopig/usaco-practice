ac974ee30ce535264c225a3a73a4e0092e0b175b
Vedant Garg <71409503+cryptopig@users.noreply.github.com>
2024-10-21 10:30:23 -0700

    Update Blackjack.java

--- src/easy/Blackjack.java
+++ src/easy/Blackjack.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class Blackjack {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Sytstem.out.println("Would you like to play? Y/N \n> ");
+        
+        if (input.nextString().toLowerCase() == "y") {
+            System.out.println("What is your name?\n > ");
+            String name = input.nextLine();
+        }
+        
+        else {
+            System.out.println("It's a shame you don't want to win big 🤑🤑🤑🤑🤑");
+        }
+    }
+}
