c5939d15b052fa44a6d7f67b57ff903648fd4149
Vedant Garg <71409503+cryptopig@users.noreply.github.com>
2024-10-22 09:28:48 -0700

    Update Blackjack.java

--- src/easy/Blackjack.java
+++ src/easy/Blackjack.java
@@ -1,17 +1,35 @@
 import java.util.Scanner;
 
 public class Blackjack {
+    
+    static Scanner input = new Scanner(System.in);
+    static Random rand = new Random();
+    static int playerScore,dealerScore;
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Sytstem.out.println("Would you like to play? Y/N \n> ");
-        
-        if (input.nextString().toLowerCase() == "y") {
-            System.out.println("What is your name?\n > ");
+        System.out.print("Would you like to play? Y/N \n> ");
+        if (input.next().toLowerCase().equals("y")) {
+            System.out.print("What is your name?\n> ");
             String name = input.nextLine();
+            System.out.println("Hello, " + name + ".")
         }
         
         else {
-            System.out.println("It's a shame you don't want to win big 🤑🤑🤑🤑🤑");
+            System.out.println("It's a shame you don't want to win big...");
         }
+    }
+    
+    public static int hit() {
+        int n = rand.nextInt(12-2) + 2; // lowest value is 2, highest is 11
+        // give choice of 1 to 11, check if the user inputs something else
+        if (n == 11) {
+            while (n == 1 || n == 11) {
+                System.out.print("Do you want a 1 or 11?");
+                int value = input.nextInt();
+                n = value;
+            }
+        }
+        
+        return n;
     }
 }
