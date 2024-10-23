f6903f2f1e81851a844e8e4708db6c2e4b577344
Vedant Garg <71409503+cryptopig@users.noreply.github.com>
2024-10-23 09:48:50 -0700

    Update Blackjack.java

--- src/easy/Blackjack.java
+++ src/easy/Blackjack.java
@@ -10,84 +10,85 @@ public class Blackjack {
     public static void main(String[] args) {
         double gains = 0;
         while (true) {    
-            System.out.println("Would you like to play? Y/N");
+            System.out.println("Would YOU like to play? Y/N");
             // if user inputs anything except for y
             if (!input.next().equalsIgnoreCase("y")) {
-                System.out.println("It's a shame you don't want to win big...");
+                System.out.println("It's a shame YOU don't want to win big...");
                 // exits the program forcefully, so while loop doesn't run
                 System.exit(0);
             }
-    
-            System.out.println("How much do you want to wager?");
-            double wager = input.nextDouble();
+
+            double wager = 0;
+            while (!(wager > 0)) {
+                System.out.println("How much do YOU want to wager? It must be a bet greater than 0.");
+                wager = input.nextDouble();
+            }
+
             // loop to deal out the hand
             for (int i = 1; i <= 2; i++) {
                 int card = hit(playerScore);
                 playerScore += card;
-    
                 card = hit(dealerScore);
                 dealerScore += card;
                 if (i == 1) {
-                    System.out.println("Dealer's face-up card: " + card);
+                    System.out.println("DEALER'S face-up card: " + card);
                 }
             }
-            System.out.println("Your hand total: " + playerScore);
-    
+            System.out.println("YOUR hand total: " + playerScore);
             while (playerScore < 21) {
                 String choice;
-                System.out.println("Do you want to Hit [H] or Stand [S]?");
+                System.out.println("Do YOU want to Hit [H] or Stand [S]?");
                 choice = input.next();
                 // takes care of all input besides S
                 if (!choice.equalsIgnoreCase("s")) {
                     playerScore += hit(playerScore);
-                    System.out.println("Your hand total: " + playerScore);
+                    System.out.println("YOUR hand total: " + playerScore);
                 }
-                
                 // if the player stands, it leaves the loop
                 else {
-                    System.out.println("Dealers turn! The game ends when the dealer stands.");
+                    System.out.println("DEALER'S turn! The game ends when the DEALER stands.");
                     break;
                 }
             }
             while (dealerScore < 17) {
-                dealerScore += hit(dealerScore);
+                int card = hit(dealerScore);
+                dealerScore += card;
+                System.out.println("DEALER draws " + card);
             }
-    
+
             if (playerScore > 21) {
                 System.out.println("\nBust! Better luck next time.\n");
                 gains += outcome(wager, "loss");
-                System.out.println("Your gains/losses: " + gains);
+                System.out.println("YOUR gains/losses: " + "$" + gains);
             } else if (dealerScore > 21 ) {
-                System.out.println("\nThe dealer bust! You won.\n");
+                System.out.println("\nThe dealer bust! YOU won.\n");
                 gains += outcome(wager, "win");
-                System.out.println("Your gains/losses: " + gains);
+                System.out.println("YOUR gains/losses: " + "$" + gains);
     
             } else if (playerScore == dealerScore) {
                 System.out.println("\nTie game!\n");
                 gains += outcome(wager, "tie");
-                System.out.println("Your gains/losses: " + gains);
+                System.out.println("YOUR gains/losses: " + "$" + gains);
             } else if (playerScore == 21) {
-                System.out.println("\n21! You win.\n");
+                System.out.println("\n21! YOU win.\n");
                 gains += outcome(wager, "win");
-                System.out.println("Your gains/losses: " + gains);
+                System.out.println("YOUR gains/losses: " + "$" + gains);
             } else if(dealerScore > playerScore) {
                 System.out.println("\nDealer won! Better luck next time.\n");
                 gains += outcome(wager, "loss");
-                System.out.println("Your gains/losses: " + gains);
+                System.out.println("YOUR gains/losses: " + "$" + gains);
             } else {
-                System.out.println("\nYou won!\n");
+                System.out.println("\nYOU won!\n");
                 gains += outcome(wager, "win");
-                System.out.println("Your gains/losses: " + gains);
+                System.out.println("YOUR gains/losses: " + "$" + gains);
             }
-    
-            System.out.println("Your hand total: " + playerScore);
-            System.out.println("Dealer's hand total: " + dealerScore + "\n");
+            System.out.println("YOUR hand total: " + playerScore);
+            System.out.println("DEALER'S hand total: " + dealerScore + "\n");
             System.out.println("___________________________\n");
             playerScore = 0;
             dealerScore = 0;
         }
     }
-
     // drawing cards
     public static int hit(int score) {
         int n = rand.nextInt(13-2) + 1; // lowest value is 2, highest is 13
@@ -100,18 +101,16 @@ public class Blackjack {
         }
         return n;
     }
-
    // function to handle the wager for every outcome of the game
    public static double outcome(double wager, String outcome) {
         if (Objects.equals(outcome, "win")) {
             return 2*wager;
         }
-        if (Objects.equals(outcome, "draw")) {
+        if (Objects.equals(outcome, "tie")) {
             return wager * 0;
         }
         else {
             return -wager;
         }
-    
    }
 }
