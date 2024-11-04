e4183d969f740616106a92979aa603d95af335d6
Vedant Garg <71409503+cryptopig@users.noreply.github.com>
2024-11-04 15:24:23 -0800

    Update Basketball.java

--- src/easy/Basketball.java
+++ src/easy/Basketball.java
@@ -1,1 +1,10 @@
-
+// https://open.kattis.com/problems/basketballoneonone
+import java.util.Scanner;
+public class Basketball {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String record = input.nextLine();
+        char lastTurn = record.charAt(record.length() - 2);
+        System.out.println(lastTurn);
+    }
+}
