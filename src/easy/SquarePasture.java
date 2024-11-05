09f8b293f1a7b190f87ae99c566b2ad57338742c
Vedant Garg <71409503+cryptopig@users.noreply.github.com>
2024-11-05 13:26:45 -0800

    Update SquarePasture.java

--- src/easy/SquarePasture.java
+++ src/easy/SquarePasture.java
@@ -1,26 +1,18 @@
-import java.util.Scanner;
-public class SquarePasture {
+import java.util.*;
+class SquarePasture {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        // storing coordinates for each pasture in two arrays.
-        int[] pasture1 = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
+        Dictionary<String, Integer> pasture_1 = new Hashtable<>();
+        pasture_1.put("x1", input.nextInt());
+        pasture_1.put("y1", input.nextInt());
+        pasture_1.put("x2", input.nextInt());
+        pasture_1.put("y2", input.nextInt());
         
-        int[] pasture2 = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
-        
-        // store side lengths in another  array
-        int[] pasture1_lengths = {(pasture1[3] - pasture1[1]), (pasture1[2] - pasture1[0])};
-        int[] pasture2_lengths = {(pasture2[3] - pasture2[1]), (pasture2[2] - pasture2[0])};
-        
-        // finding the largest side length
-    
-        int side_length = 
-        Math.max(
-            Math.max(
-                pasture1_lengths[0], pasture1_lengths[1]
-            );
-            
-        );
-        
+        Dictionary<String, Integer> pasture_2 = new Hashtable<>();
+        pasture_2.put("x1", input.nextInt());
+        pasture_2.put("y1", input.nextInt());
+        pasture_2.put("x2", input.nextInt());
+        pasture_2.put("y2", input.nextInt());
         
     }
 }
