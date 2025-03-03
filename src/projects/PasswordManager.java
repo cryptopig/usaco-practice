9246fadc0fa120e1e91ccad452adae872ab3efc5
Vedant Garg <71409503+cryptopig@users.noreply.github.com>
2025-03-03 23:44:07 +0000

    pw manager

--- src/projects/PasswordManager.java
+++ src/projects/PasswordManager.java
@@ -1,30 +1,48 @@
 public class PasswordManager {
+    public static void main(String[] args) {
+        PasswordGenerator generator = new PasswordGenerator();
+        Password password = generator.generate_password(8);
+        System.out.println(password.returnPassword());
+
+       
+    }
 
 }
 
+
 // pw object to make it easier to deal with 
 class Password {
-    private int length = 12; // default length = 12
     private String contents = "1s(.jQ]#145v"; // default password = 1s(.jQ]#145v
 
-    public Password(int length, String contents) {
-        this.length = length;
+    public Password(String contents) {
         this.contents = contents;
+    }
+
+    public String returnPassword() {
+        return this.contents;
     }
 }
 
 class PasswordGenerator {
     private int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    private String letters = "qwertyuiopasdfghjklzxcvbnm";
+    private static String letters = "QqwWeErRtTyYuUiIoOpPaAsSdDfFgGhHjJkKlLZzXxCcVvBbNnMm";
     private String characters = "!@#$%^&*()_+{}|:<>?,.-=[]";
 
-    public static Passsword generate_password(int length) {
+    public Password generate_password(int length) {
         String contents = "";
+        for (int i = 0; i < length; i++) {
+            contents += random(0, letters.length());
+        }
 
 
         Password generatedPassword = new Password(length, contents);
         return generatedPassword;
     }
+
+    public int random(int min, int max) {
+        return (rand.nextInt(max - min + 1) + min);
+    }
+
 }
 
 /* outline:
